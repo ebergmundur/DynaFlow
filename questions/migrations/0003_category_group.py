26ed069d675014821f14afd8e1942617eb2acfa6
Einar Bergmundur <einar@nature.is>
2020-06-05 13:25:55 +0000

    Category

--- questions/migrations/0003_category_group.py
+++ questions/migrations/0003_category_group.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        # ('questions', '0002_auto_20200605_1300'),
+        ('questions', '0002_auto_20200605_1300'),
     ]
 
     operations = [
