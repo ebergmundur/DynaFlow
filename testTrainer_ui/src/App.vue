544e1f818eda5982a0f7868df362ddc7762a5da0
Einar Bergmundur <einar@nature.is>
2020-12-01 16:37:32 +0000

    callback handling

--- testTrainer_ui/src/App.vue
+++ testTrainer_ui/src/App.vue
@@ -5,8 +5,46 @@
 </template>
 
 <script>
+import AuthService from './../auth/AuthService'
+import axios from 'axios'
+
+const API_URL = 'http://api.enam.is'
+const auth = new AuthService()
 
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    this.handleAuthentication()
+    this.authenticated = false
+
+    auth.authNotifier.on('authChange', authState => {
+      this.authenticated = authState.authenticated
+    })
+
+    return {
+      authenticated: false,
+      message: ''
+    }
+  },
+  methods: {
+    // this method calls the AuthService login() method
+    login () {
+      auth.login()
+    },
+    handleAuthentication () {
+      auth.handleAuthentication()
+    },
+    logout () {
+      auth.logout()
+    },
+    privateMessage () {
+      const url = `${API_URL}/api/private/`
+      return axios.get(url, { headers: { Authorization: `Bearer ${auth.getAuthToken()}` } }).then((response) => {
+        console.log(response.data)
+        this.message = response.data || ''
+      })
+    }
+  }
 }
+
 </script>
--- testTrainer_ui/src/layouts/MainLayout.vue
+++ testTrainer_ui/src/layouts/MainLayout.vue
@@ -178,15 +178,15 @@
 // ]
 
 import Clock from 'components/Clock'
-import AuthService from './../auth/AuthService'
+// import AuthService from './../auth/AuthService'
 import axios from 'axios'
 
-const API_URL = 'http://test.enam.is:8000'
-const auth = new AuthService()
+// const API_URL = 'http://api.enam.is'
+// const auth = new AuthService()
 
 export default {
   name: 'MainLayout',
-  components: { Clock },
+  components: { Clock }
   // components: { Questions },
   // data () {
   //   return {
@@ -194,37 +194,37 @@ export default {
   //     rightDrawer: false
   //   }
   // }
-  data () {
-    this.handleAuthentication()
-    this.authenticated = false
-
-    auth.authNotifier.on('authChange', authState => {
-      this.authenticated = authState.authenticated
-    })
-
-    return {
-      authenticated: false,
-      message: ''
-    }
-  },
-  methods: {
-    // this method calls the AuthService login() method
-    login () {
-      auth.login()
-    },
-    handleAuthentication () {
-      auth.handleAuthentication()
-    },
-    logout () {
-      auth.logout()
-    },
-    privateMessage () {
-      const url = `${API_URL}/api/private/`
-      return axios.get(url, { headers: { Authorization: `Bearer ${auth.getAuthToken()}` } }).then((response) => {
-        console.log(response.data)
-        this.message = response.data || ''
-      })
-    }
-  }
+  // data () {
+  //   this.handleAuthentication()
+  //   this.authenticated = false
+  //
+  //   auth.authNotifier.on('authChange', authState => {
+  //     this.authenticated = authState.authenticated
+  //   })
+  //
+  //   return {
+  //     authenticated: false,
+  //     message: ''
+  //   }
+  // },
+  // methods: {
+  //   // this method calls the AuthService login() method
+  //   login () {
+  //     auth.login()
+  //   },
+  //   handleAuthentication () {
+  //     auth.handleAuthentication()
+  //   },
+  //   logout () {
+  //     auth.logout()
+  //   },
+  //   privateMessage () {
+  //     const url = `${API_URL}/api/private/`
+  //     return axios.get(url, { headers: { Authorization: `Bearer ${auth.getAuthToken()}` } }).then((response) => {
+  //       console.log(response.data)
+  //       this.message = response.data || ''
+  //     })
+  //   }
+  // }
 }
 </script>
