05dafd023067187657f2d8f1bd19958bc098a39e
Einar Bergmundur <einar@nature.is>
2020-12-01 16:32:47 +0000

    callback handling

--- testTrainer_ui/src/auth/AuthService.js
+++ testTrainer_ui/src/auth/AuthService.js
@@ -19,7 +19,7 @@ export default class AuthService {
   auth0 = new auth0.WebAuth({
     domain: 'dev-4z-pkajz.eu.auth0.com',
     clientID: 'QVNW3HExUygBcPkY2hfxFSXCQAss6EzG',
-    redirectUri: 'https://enam.is/#/callback',
+    redirectUri: 'https://enam.is/',
     audience: 'https://dev-4z-pkajz.eu.auth0.com/api/v2/',
     responseType: 'token id_token',
     scope: 'openid profile'
--- testTrainer_ui/src/layouts/MainLayout.vue
+++ testTrainer_ui/src/layouts/MainLayout.vue
@@ -66,7 +66,7 @@
 
       <button
         class="btn btn-primary btn-margin"
-        v-if="authenticated"
+        v-if="!authenticated"
         @click="logout()">
         Log Out
       </button>
