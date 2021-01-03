55e50cea8cf0e9d261125d8060a58f275128be94
Einar Bergmundur <einar@nature.is>
2021-01-03 14:09:17 +0000

    No blacklist

--- TestTrainer/settings.py
+++ TestTrainer/settings.py
@@ -249,7 +249,7 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=240),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': True,
+    'BLACKLIST_AFTER_ROTATION': False,
     'UPDATE_LAST_LOGIN': False,
 
     'ALGORITHM': 'HS256',
